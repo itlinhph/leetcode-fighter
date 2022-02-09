7c7df773ba2ef1a569d67d8fdd1c74e4f2710584
itlinhph <itlinhph@ghtk.co>
2022-02-10 00:38:17 +0700

    trap rain water dinamic programing

--- 3_trapping-rain-water.py
+++ 3_trapping-rain-water.py
@@ -29,20 +29,46 @@ class Solution:
         for i, item in enumerate(height):
             if i == 0 or i == len(height) - 1:
                 continue
+            # find max heigt in both left and right
             left_max = max(height[:i])
             right_max = max(height[i+1:])
 
             water = min(left_max, right_max) - item
-            if water < 0:
-                water = 0
-            total_water += water
+            if water > 0:
+                total_water += water
         return total_water
-            
+
+    def trap_dinamic_programing(height: List[int]) -> int:
+        total_water = 0
+        
+        # find left max
+        left_max = [0]*len(height)
+        current_max = height[0]
+        for i in range(1, len(height)):
+            if height[i-1] > current_max:
+                current_max = height[i-1]
+            left_max[i] = current_max
+        
+        # find right max
+        right_max = [0]*len(height)
+        current_max = height[-1]
+        for i in reversed(range(len(height)-1)):
+            if height[i+1] > current_max:
+                current_max = height[i+1]
+            right_max[i] = current_max
+        
+        # sum water
+        for i, item in enumerate(height):
+            water = min(left_max[i], right_max[i]) - item
+            if water > 0:
+                total_water += water
+
+        return total_water
 
 
 
-height = [4,2,0,3,2,5] # should return 9
+# height = [4,2,0,3,2,5] # should return 9
 height = [0,1,0,2,1,0,1,3,2,1,2,1]  # should return 6
 
-x = Solution.trap_v1(height=height)
+x = Solution.trap_dinamic_programing(height=height)
 print(x)
